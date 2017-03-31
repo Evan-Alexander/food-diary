a6e8e1e4b35198bda989222654403a02ed533580
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 16:59:13 -0700

    add service within home.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Food } from '../food.model';
 import { Router } from '@angular/router';
+import { FoodService } from '../food.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [FoodService]
 })
 export class HomeComponent {
-
+  foods: Food[];
 
   goToDetailPage(clickedFood: Food) {
     this.router.navigate(['foods', clickedFood.id]);
   }
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private foodService: FoodService
+  ) { }
 
 }
