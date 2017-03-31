4f3eeb4b3bba77973455e6bc0d2fcd55131187ca
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 16:49:26 -0700

    move food array

---
+++ src/app/mock-foods.ts
@@ -0,0 +1,7 @@
+import { Food } from './food.model';
+
+export const FOODS: Food[] = [
+  new Food("Cliff Bar", "Peanut Butter", 250, 1),
+  new Food("Tacos", "Burrito Axteca chicken", 300, 2),
+  new Food("Tortilla Chips", "With salsa", 350, 3),
+];
