6904e1688cfb8ed4241d5fd25d26c3d07fbd1490
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 16:39:15 -0700

    add food detail component and path

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,16 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FoodComponent } from './food/food.component';
+import { FoodDetailComponent }   from './food-detail/food-detail.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+   path: 'food/:id',
+   component: FoodDetailComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
