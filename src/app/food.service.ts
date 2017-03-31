53cbb6800adb14c4bfbba154bd14d0961d6c13a5
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 16:58:47 -0700

    add service to class

---
+++ src/app/food.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Food } from './food.model';
+import { FOODS } from './mock-foods';
+
+@Injectable()
+export class FoodService {
+
+  constructor() { }
+  getFoods() {
+    return FOODS;
+  }
+}
