d7eb08f45f19e403d951234d636c5452c85da884
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 15:54:33 -0700

    add imports for food models and food component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Food } from '../food.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  foods: Food[] = [
+    new Food("Cliff Bar", "Peanut Butter", 250, 1),
+    new Food("Tacos", "Burrito Axteca chicken", 300, 2),
+    new Food("Tortilla Chips", "With salsa", 350, 3),
+  ];
   constructor() { }
 
   ngOnInit() {
