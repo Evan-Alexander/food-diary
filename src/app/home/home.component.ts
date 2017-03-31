a4fa7318e96bceba22ac398660191efc17415ce3
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 16:48:50 -0700

    move array to mock-foods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  foods: Food[] = [
-    new Food("Cliff Bar", "Peanut Butter", 250, 1),
-    new Food("Tacos", "Burrito Axteca chicken", 300, 2),
-    new Food("Tortilla Chips", "With salsa", 350, 3),
-  ];
+
 
   goToDetailPage(clickedFood: Food) {
     this.router.navigate(['foods', clickedFood.id]);
