6bed1b81ab183584ae40f1a80b4985feed78445e
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 21:23:08 -0700

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { FoodService } from './food.service';
 import { AngularFireModule } from 'angularfire2';
-import { masterFirebaseConfig } from './api-keys';
+// import { masterFirebaseConfig } from './api-keys';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { routing } from './app.routing';
+import { FoodService } from './food.service';
 import { FoodDetailComponent } from './food-detail/food-detail.component';
 import { NewFoodComponent } from './new-food/new-food.component';
 import { EditFoodComponent } from './edit-food/edit-food.component';
