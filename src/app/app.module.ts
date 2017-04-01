167576da2af16bbbe14926ceca919cb43b573b35
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 19:34:08 -0700

    add edit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
 import { FoodDetailComponent } from './food-detail/food-detail.component';
 import { NewFoodComponent } from './new-food/new-food.component';
+import { EditFoodComponent } from './edit-food/edit-food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FoodDetailComponent,
-    NewFoodComponent
+    NewFoodComponent,
+    EditFoodComponent
   ],
   imports: [
     BrowserModule,
