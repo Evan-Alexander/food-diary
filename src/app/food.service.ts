e76214761474e6f564e9c0b0329110b5324a7748
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 20:42:51 -0700

    add firebase observable

--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Food } from './food.model';
 import { FOODS } from './mock-foods';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Injectable()
 export class FoodService {
-  getFoods(): Promise<Food[]> {
-    return Promise.resolve(FOODS);
-  }
-  getFood(id: number): Promise<Food> {
-    return this.getFoods().then(foods => foods.find(food => food.id === id));
-  }
-  constructor() { }
+  foods: FirebaseListObservable <any[]>;
 
+
+  getFoods(){
+    return this.foods;
+  }
+  constructor(private angularFire: AngularFire) {
+    this.foods = angularFire.database.list('foods');
+  }
 }
