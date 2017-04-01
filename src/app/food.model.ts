ca7a6c8fd80110034f4de91dd182d5f5ba6b3fba
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 21:34:49 -0700

    add key to constructor

--- src/app/food.model.ts
+++ src/app/food.model.ts
@@ -1,3 +1,3 @@
 export class Food {
-  constructor (public name: string, public description: string, public calories: number, public id: number) { }
+  constructor (public name: string, public description: string, public calories: number, public id: number, public $key: any) { }
 }
