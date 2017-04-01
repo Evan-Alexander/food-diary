5f8333c33ad5dfc10ea9e28da6f40d17ec0012cc
Jason Brown <jasontbrown99@gmail.com>
2017-03-31 21:13:50 -0700

    add functionality to update food

--- src/app/edit-food/edit-food.component.ts
+++ src/app/edit-food/edit-food.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { Food } from '../food.model';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-edit-food',
   templateUrl: './edit-food.component.html',
-  styleUrls: ['./edit-food.component.css']
+  styleUrls: ['./edit-food.component.css'],
+  providers: [FoodService]
 })
 export class EditFoodComponent implements OnInit {
-  @Input() childSelectedFood: Food;
+  @Input() selectedFood;
   @Output() doneButtonClickedSender = new EventEmitter();
 
-  doneButtonClicked() {
-    this.doneButtonClickedSender.emit();
-  }
-  constructor() { }
+  // doneButtonClicked() {
+  //   this.doneButtonClickedSender.emit();
+  // }
+  constructor(private foodService: FoodService) { }
 
   ngOnInit() {
   }
 
+  updateFood(selectedFood){
+   this.foodService.updateFood(selectedFood);
+   this.doneButtonClickedSender.emit();
+ }
 }
