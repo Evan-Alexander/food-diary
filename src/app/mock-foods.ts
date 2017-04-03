6ed5ea7779ed42d7e29d84844bc93905ce8b3e03
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 19:51:17 -0700

    comment out for now

--- src/app/mock-foods.ts
+++ src/app/mock-foods.ts
@@ -1,5 +1,5 @@
 // import { Food } from './food.model';
-//
+
 // export const FOODS: Food[] = [
 //   new Food("Cliff Bar", "Peanut Butter", 250, 1),
 //   new Food("Tacos", "Burrito Axteca chicken", 300, 2),
