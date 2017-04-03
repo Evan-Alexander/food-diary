56016f5148a340577e56260406146d309e4660be
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 21:23:22 -0700

    import and inject foodservice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../food.model';
 import { Router } from '@angular/router';
-
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [FoodService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
   foods: Food[];
 
+  constructor(private router: Router, private foodService: FoodService) { }
+
+
   ngOnInit() {
+    this.foods = this.foodService.getFoods();
   }
 
   goToEditPage(clickedFood: Food) {
