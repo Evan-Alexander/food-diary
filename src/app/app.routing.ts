99c0a4fd6ce556439f750dd13bb2560e73120b1f
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 20:04:50 -0700

    add edit-foods

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
-
+import { EditFoodsComponent } from './edit-foods/edit-foods.component';
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: '',
+    component: EditFoodsComponent
   }
 
 ];
