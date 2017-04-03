9f38ac5a50e4e86e531a5df33d189e0a871d5be7
Jason Brown <jasontbrown99@gmail.com>
2017-04-03 00:07:12 -0700

    create updatefoods

---
+++ src/app/update-foods/update-foods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-foods',
+  templateUrl: './update-foods.component.html',
+  styleUrls: ['./update-foods.component.css']
+})
+export class UpdateFoodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
