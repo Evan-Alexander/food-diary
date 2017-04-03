5f11b57355eecbfbf70a8b35f65aca25e037cda8
Jason Brown <jasontbrown99@gmail.com>
2017-04-03 00:37:09 -0700

    update form removed from editpage; not implemented yet

--- src/app/update-foods/update-foods.component.ts
+++ src/app/update-foods/update-foods.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-update-foods',
   templateUrl: './update-foods.component.html',
-  styleUrls: ['./update-foods.component.css']
+  styleUrls: ['./update-foods.component.css'],
+  providers: [FoodService]
 })
 export class UpdateFoodsComponent implements OnInit {
+  @Input() selectedFood;
 
-  constructor() { }
+  constructor(private foodService: FoodService) { }
 
   ngOnInit() {
   }
