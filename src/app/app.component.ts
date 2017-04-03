0797392eb2e2414edb8485252e7f4f29407468c3
Jason Brown <jasontbrown99@gmail.com>
2017-04-03 00:01:19 -0700

    alter app and home.html to display title once and not show calorie count on front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Food Diary';
+  title = 'Welcome to the wonderful world of calorie counting!';
 }
