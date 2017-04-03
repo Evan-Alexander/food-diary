3926b3ab74bd132f82df2f17c3f25ebb7dbad777
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 20:55:44 -0700

    remove object contents; define food object

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,12 +12,7 @@ export class HomeComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  foods: Food[] = [
-    new Food("Cliff Bar", "Peanut Butter", 250, 1),
-    new Food("Coffee", "with a touch of cream and sugar", 150, 1),
-    new Food("Tacos", "Burrito Axteca chicken", 300, 2),
-    new Food("Tortilla Chips", "With salsa", 350, 3),
-  ];
+  foods: Food[];
 
   ngOnInit() {
   }
