8a1952c92d4b5f85b06b4e6996dceaeec9153f34
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 20:13:19 -0700

    import router, add router to constructor, and function to display editpage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../food.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,15 +9,21 @@ import { Food } from '../food.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
   foods: Food[] = [
     new Food("Cliff Bar", "Peanut Butter", 250, 1),
     new Food("Coffee", "with a touch of cream and sugar", 150, 1),
     new Food("Tacos", "Burrito Axteca chicken", 300, 2),
     new Food("Tortilla Chips", "With salsa", 350, 3),
   ];
-  constructor() { }
 
   ngOnInit() {
+  }
+
+  goToEditPage(clickedFood: Food) {
+    this.router.navigate(['foods', clickedFood.id]);
   }
 
 }
