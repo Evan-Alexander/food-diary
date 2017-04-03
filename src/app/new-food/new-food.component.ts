0b80b9e450bd8deff72ac18cf7b8401c6f3d63db
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 23:50:47 -0700

    add submitform function

--- src/app/new-food/new-food.component.ts
+++ src/app/new-food/new-food.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { FoodService } from '../food.service'
+import { Food } from '../food.model';
 
 @Component({
   selector: 'app-new-food',
   templateUrl: './new-food.component.html',
-  styleUrls: ['./new-food.component.css']
+  styleUrls: ['./new-food.component.css'],
+  providers: [FoodService]
 })
 export class NewFoodComponent implements OnInit {
+  foods: FirebaseObjectObservable<any[]>;
 
-  constructor() { }
+  constructor(private foodService: FoodService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(name: string, description: string, calories: string) {
+    var newFood: Food = new Food(name, description, calories);
+    this.foodService.addFood(newFood);
   }
 
 }
