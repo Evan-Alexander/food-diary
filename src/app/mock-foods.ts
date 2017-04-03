24f179b00250d081442584a47583d515c098062d
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 21:22:40 -0700

    change id on food items

--- src/app/mock-foods.ts
+++ src/app/mock-foods.ts
@@ -2,7 +2,7 @@ import { Food } from './food.model';
 
 export const FOODS: Food[] = [
   new Food("Cliff Bar", "Peanut Butter", 250, 1),
-  new Food("Coffee", "with a touch of cream and sugar", 150, 1),
-  new Food("Tacos", "Burrito Axteca chicken", 300, 2),
-  new Food("Tortilla Chips", "With salsa", 350, 3),
+  new Food("Coffee", "with a touch of cream and sugar", 150, 2),
+  new Food("Tacos", "Burrito Axteca chicken", 300, 3),
+  new Food("Tortilla Chips", "With salsa", 350, 4),
 ];
