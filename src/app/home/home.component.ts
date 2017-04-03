6d7993955339222785407417a9add26d43edec4c
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 19:53:21 -0700

    add food object for initial testing of app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Food } from '../food.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  foods: Food[] = [
+    new Food("Cliff Bar", "Peanut Butter", 250, 1),
+    new Food("Coffee", "with a touch of cream and sugar", 150, 1),
+    new Food("Tacos", "Burrito Axteca chicken", 300, 2),
+    new Food("Tortilla Chips", "With salsa", 350, 3),
+  ];
   constructor() { }
 
   ngOnInit() {
