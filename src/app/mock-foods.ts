4986421844500af53f8f87f0e038073673ad4f35
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 23:48:41 -0700

    comment out for now

--- src/app/mock-foods.ts
+++ src/app/mock-foods.ts
@@ -1,8 +1,8 @@
-import { Food } from './food.model';
-
-export const FOODS: Food[] = [
-  new Food("Cliff Bar", "Peanut Butter", 250, 1),
-  new Food("Coffee", "with a touch of cream and sugar", 150, 2),
-  new Food("Tacos", "Burrito Axteca chicken", 300, 3),
-  new Food("Tortilla Chips", "With salsa", 350, 4),
-];
+// import { Food } from './food.model';
+//
+// export const FOODS: Food[] = [
+//   new Food("Cliff Bar", "Peanut Butter", 250, 1),
+//   new Food("Coffee", "with a touch of cream and sugar", 150, 2),
+//   new Food("Tacos", "Burrito Axteca chicken", 300, 3),
+//   new Food("Tortilla Chips", "With salsa", 350, 4),
+// ];
