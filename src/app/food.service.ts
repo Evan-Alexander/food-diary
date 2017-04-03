1a0fd3f7625f277e7c5dad05348738d4a203b163
Jason Brown <jasontbrown99@gmail.com>
2017-04-02 21:06:46 -0700

    import model and mock-foods; add getfoods function

--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { FOODS } from './mock-foods';
+import { Food } from './food.model';
 
 @Injectable()
+
 export class FoodService {
 
   constructor() { }
+
+  getFoods() {
+    return FOODS;
+  }
 
 }
