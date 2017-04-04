8a15d02503fef297e95505aed89cbdabb7623e88
Jason Brown <jasontbrown99@gmail.com>
2017-04-03 20:20:25 -0700

    rename app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to the wonderful world of calorie counting!';
+  title = 'Food Diary';
 }
